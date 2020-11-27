ba2b5f72656fac5af9611d0d1b6863b42892ec81
oscar salazar <salazaroscar9705@gmail.com>
2020-11-27 17:29:36 -0500

    veterinaryBloc

--- lib/src/bloc/veterinary_bloc.dart
+++ lib/src/bloc/veterinary_bloc.dart
@@ -1,20 +1,23 @@
 import 'dart:async';
 
 import 'package:demo/src/repository/veterinary_repository.dart';
+import 'package:demo/src/utils/apiresponse_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:progress_dialog/progress_dialog.dart';
 
 import '../model/veterinary_model.dart';
 
 class VeterinaryBloc {
-  BuildContext _context;
-  Veterinary _veterinary;
-  ProgressDialog _progressDialog;
   final _repository = VeterinaryRepository();
+  var _apiResponse = ApiResponse();
+  BuildContext _context;
+  ProgressDialog _progressDialog;
+  final _veterinaryListController =
+      StreamController<List<Veterinary>>.broadcast();
   final _veterinaryController = StreamController<Veterinary>.broadcast();
 
   VeterinaryBloc(BuildContext context) {
     _context = context;
-    _veterinary = Veterinary();
+    _apiResponse = ApiResponse();
   }
 }
