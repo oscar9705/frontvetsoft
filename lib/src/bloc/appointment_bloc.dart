dda7e4d81c977209a1ac3cd48dc8fe3b0bf9c338
ABarrero <andresbarrerosky@hotmail.es>
2020-11-30 15:32:18 -0500

    CRUD Methods Added to Appontment Bloc and little fixes

--- lib/src/bloc/appointment_bloc.dart
+++ lib/src/bloc/appointment_bloc.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:demo/src/model/appointment_model.dart';
 import 'package:demo/src/repository/appointment_respository.dart';
 import 'package:demo/src/utils/apiresponse_model.dart';
+import 'package:demo/src/utils/errorapiresponse_model.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:progress_dialog/progress_dialog.dart';
+import 'package:demo/src/resource/Constants.dart';
 
 class AppointmentBloc {
   final _repository = AppointmentRepository();
@@ -13,7 +13,8 @@ class AppointmentBloc {
 
   Appointment _appointment;
   BuildContext _context;
-  ProgressDialog _progressDialog;
+  ErrorApiResponse _error;
+
   final _appointmentListController =
       StreamController<List<Appointment>>.broadcast();
   final _appointmentController = StreamController<Appointment>.broadcast();
@@ -22,5 +23,60 @@ class AppointmentBloc {
   AppointmentBloc(BuildContext context) {
     _context = context;
     _apiResponse = ApiResponse();
+  }
+
+  Future<ApiResponse> createAppointment(Appointment appointment) async {
+    _apiResponse = await _repository.insertAppointment(appointment);
+    if (_apiResponse.statusResponse == 200) {
+      _apiResponse.message = Constants.insertSuccess;
+    } else {
+      _apiResponse.message = Constants.insertError;
+    }
+    return _apiResponse;
+  }
+
+  Future getById(String id) async {
+    _apiResponse = await _repository.getAppointmentById(id);
+    if (_apiResponse.statusResponse == 200) {
+      _appointment = _apiResponse.object;
+    } else {
+      ErrorApiResponse error = _apiResponse.object;
+    }
+    return _apiResponse;
+  }
+
+  Future<ApiResponse> getAll() async {
+    _apiResponse = await _repository.getAllAppointments();
+    if (_apiResponse.statusResponse == 200) {
+      _appointment = _apiResponse.object;
+    } else {
+      ErrorApiResponse error = _apiResponse.object;
+    }
+    return _apiResponse;
+  }
+
+  Future<ApiResponse> updateAppointment(Appointment appointment) async {
+    _apiResponse = await _repository.updateAppointment(appointment);
+    if (_apiResponse.statusResponse == 200) {
+      _apiResponse.message = Constants.updateSuccess;
+    } else {
+      ErrorApiResponse error = _apiResponse.object;
+    }
+    return _apiResponse;
+  }
+
+  Future<ApiResponse> deleteAppointment(String id) async {
+    _apiResponse = await _repository.deleteAppointmentById(id);
+    if (_apiResponse.statusResponse == 200) {
+      _apiResponse.message = Constants.deleteSuccess;
+    } else {
+      ErrorApiResponse error = _apiResponse.object;
+    }
+    return _apiResponse;
+  }
+
+  void dispose() {
+    _appointmentController.close();
+    _appointmentListController.close();
   }
 }
--- lib/src/resource/Constants.dart
+++ lib/src/resource/Constants.dart
@@ -1,38 +1,47 @@
 class Constants {
-  static const urlAuthority = 'urlAuthority';
+  static const urlAuthority = "urlAuthority";
 
   //API User
-  static const urlFindAllUsers = 'user/all';
-  static const urlFindByIdUser = 'user/id';
-  static const urlInsertUser = 'user/save';
-  static const urlUpdateUser = 'user/update';
-  static const urlDeleteUser = 'user/delete';
+  static const urlFindAllUsers = "user/all";
+  static const urlFindByIdUser = "user/id";
+  static const urlInsertUser = "user/save";
+  static const urlUpdateUser = "user/update";
+  static const urlDeleteUser = "user/delete";
 
   //API Appointment
-  static const urlFindAllApponintments = 'appointment/all';
-  static const urlInsertApponintment = 'appointment/save';
-  static const urlFindByIdApponintment = 'appointment/id';
-  static const urlUpdateApponintment = 'appointment/update';
-  static const urlDeleteApponintment = 'appointment/delete';
+  static const urlFindAllApponintments = "appointment/all";
+  static const urlInsertApponintment = "appointment/save";
+  static const urlFindByIdApponintment = "appointment/id";
+  static const urlUpdateApponintment = "appointment/update";
+  static const urlDeleteApponintment = "appointment/delete";
 
   //API Veterinary
-  static const urlFindAllVeterinarys = 'veterinary/all';
-  static const urlInsertVeterinary = 'veterinary/save';
-  static const urlFindByIdVeterinary = 'veterinary/id';
-  static const urlUpdateVeterinary = 'veterinary/update';
-  static const urlDeleteVeterinary = 'veterinary/delete';
+  static const urlFindAllVeterinarys = "veterinary/all";
+  static const urlInsertVeterinary = "veterinary/save";
+  static const urlFindByIdVeterinary = "veterinary/id";
+  static const urlUpdateVeterinary = "veterinary/update";
+  static const urlDeleteVeterinary = "veterinary/delete";
 
   //API Pet
-  static const urlFindAllPets = 'pet/all';
-  static const urlInsertPet = 'pet/save';
-  static const urlFindByIdPet = 'pet/id';
-  static const urlUpdatePet = 'pet/update';
-  static const urlDeletePet = 'pet/delete';
+  static const urlFindAllPets = "pet/all";
+  static const urlInsertPet = "pet/save";
+  static const urlFindByIdPet = "pet/id";
+  static const urlUpdatePet = "pet/update";
+  static const urlDeletePet = "pet/delete";
+  //BranchOffice
 
-  //API Branch Office
-  static const urlFindAllBranchOffices = 'boffice/all';
-  static const urlInsertBranchOffice = 'boffice/save';
-  static const urlFindByIdBranchOffice = 'boffice/id';
-  static const urlUpdateBranchOffice = 'boffice/update';
-  static const urlDeletePBranchOffice = 'boffice/delete';
+  static const urlFindAllBranchOffice = "branchoffice/all";
+  static const urlInsertBranchOffice = "branchoffice/save";
+  static const urlFindByIdBranchOffice = "branchoffice/id";
+  static const urlUpdateBranchOffice = "branchoffice/update";
+  static const urlDeleteBranchOffice = "branchoffice/delete";
+
+  //Message
+  static const insertSuccess = "Insertado exitosamente";
+  static const updateSuccess = "Actualizado exitosamente";
+  static const deleteSuccess = "Eliminado exitosamente";
+  static const searchSuccess = "la busqueda fue exitosa";
+
+  //Error
+  static const insertError = "Error al insertar";
 }
